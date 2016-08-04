e09439e295826d2c78a3f223bdf75c980ff2d839
Joel Varland <jvarland@expedia.com>
2016-08-04 09:07:15 -0700

    added new sendmail and PullRequest class to hold data for pullrequests

--- StashNagSvc/Shared/Utils.cs
+++ StashNagSvc/Shared/Utils.cs
@@ -1,23 +1,12 @@
 ﻿using CompassLogger;
-using StashNag.Shared;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.Composition;
 using System.Configuration;
-using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
 using System.Net.Mail;
 using System.Net.Mime;
 using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading;
-using HipchatApiV2;
 using ServiceStack;
-using System.Net.Http;
 using System.Net;
-using System.IO;
-using System.Threading.Tasks;
 
 namespace StashNag.Shared
 {
@@ -54,6 +43,70 @@ namespace StashNag.Shared
 
             string responseAsText = Encoding.Default.GetString(response.GetResponseStream().ReadFully());
             return responseAsText != "{\"status\":200,\"holidays\":[]}";
+        }
+
+        internal static void SendEmail(List<PullRequest> pullRequests)
+        {
+            string SmtpServer = ConfigurationManager.AppSettings["SmtpServer"];
+            string FromEmailAddress = ConfigurationManager.AppSettings["FromEmailAddress"];
+            string ToEmailAddress = ConfigurationManager.AppSettings["ToEmailAddress"];
+            string subject = "Stale Pull Requests Need Action";
+            string nagTime = TimeSpan.Parse(ConfigurationManager.AppSettings["NagTimeSpan"]).TotalDays.ToString();
+
+            if (Environment.UserInteractive)
+            {
+                subject = $"Debug - {subject}";
+            }
+
+            List<string> emailErrorMessages = new List<string>();
+            List<string> errorMessages = new List<string>();
+            int errorCount = 0;
+            foreach (PullRequest pullRequest in pullRequests)
+            {
+                string errorBackgroundColor = errorCount % 2 == 0 ? "FFFFFF" : "EEEEEE";
+                StringBuilder emailErrorMessage = new StringBuilder();
+                errorMessages.Add($"Repository:{pullRequest.Repository}, Project:{pullRequest.Project}, PR:{pullRequest.PRUrl}, LastActiveDate: {pullRequest.LastActiveDate}");
+
+                emailErrorMessage.Append($"<div style='background-color:#{errorBackgroundColor};border:1px dotted gray; margin-bottom: .25em; ppadding:.25em'>");
+                emailErrorMessage.Append($"<p style='margin: .25em'><strong>{pullRequest.Repository} - {pullRequest.Project}<br></strong></p>");
+                emailErrorMessage.Append($"<p style='margin: .25em'><a href='{pullRequest.PRUrl}'>{pullRequest.PRUrl}</a>");
+                emailErrorMessage.Append($"<br>Last Active: {pullRequest.LastActiveDate}</p>");
+                emailErrorMessage.Append($"</div>");
+                emailErrorMessages.Add(emailErrorMessage.ToString());
+
+                errorCount++;
+            }
+
+            Logger.Error($"The following {pullRequests.Count} Pull Requests were found to be stale:\n{string.Join(Environment.NewLine, errorMessages)}");
+
+            string body = "<div style='margin: 0;font-size:14px;line-height:17px;color:#1F497D;font-family:Arial, Helvetica, sans-serif'>" +
+                            $"<p style='margin: 0;font-size: 22px;line-height: 26px'><strong>{subject}</strong></p>" +
+                            $"<p style='margin: 0;'>The following {pullRequests.Count} Pull Requests haven't had activity for at least {nagTime} days<br><br></p>" +
+                            string.Join("", emailErrorMessages) + "</div>";
+
+            MailMessage mail = new MailMessage()
+            {
+                From = new MailAddress(FromEmailAddress),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            };
+            mail.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(body, null, MediaTypeNames.Text.Html));
+            mail.To.Add(new MailAddress(ToEmailAddress));
+
+            try
+            {
+                using (SmtpClient smpt = new SmtpClient())
+                {
+                    smpt.Host = SmtpServer;
+                    smpt.Timeout = 15000;
+                    smpt.Send(mail);
+                }
+            }
+            catch (Exception e)
+            {
+                Logger.Error($"Unable to send mail. {e.ToString()}");
+            }
         }
 
     }
--- StashNagSvc/StashNag.cs
+++ StashNagSvc/StashNag.cs
@@ -4,13 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Configuration;
-using System.Data;
-using System.Data.SqlClient;
 using System.Linq;
-using System.Net.Mail;
-using System.Net.Mime;
-using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading;
 
 namespace StashNag
@@ -20,9 +14,8 @@ namespace StashNag
     public class StashNag : ITaskEngine
     {
         //private static int EventId = 1000;
-        public static readonly string SmtpServer = ConfigurationManager.AppSettings["SmtpServer"];
-        public static readonly string FromEmailAddress = ConfigurationManager.AppSettings["FromEmailAddress"];
-        public static readonly string ToEmailAddress = ConfigurationManager.AppSettings["ToEmailAddress"];
+        public static readonly string hipChatToken = ConfigurationManager.AppSettings["HipChatToken"];
+        public static readonly string hipChatRoomName = ConfigurationManager.AppSettings["HipChatRoomName"];
 
         public void executeStashNag(string stashUrl, CancellationToken processToken)
         {
@@ -45,60 +38,21 @@ namespace StashNag
                             continue;
                         }
 
-                        //TODO: Get the Stash data here - stash API call and json data
+                        List<string> repoUrls = ConfigurationManager.AppSettings["StashUrls"].ToString().Split(new[] { '|', ',' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                        foreach (string repoUrl in repoUrls)
+                        {
+                            Logger.Info($"Checking {repoUrl}...");
 
-                        //TODO: If StashCheck returned PR's to alert on, send report (email and/or hipchat)
+                            // Begin Replace with Stash Checking Code
+                            List<PullRequest> pullRequests = new List<PullRequest>();
+                            pullRequests.Add(new PullRequest("eCIS Systems Automation", "Compass", "https://stash/projects/ESA/repos/compass/pull-requests/911/overview", DateTime.Now.AddDays(-5)));
+                            pullRequests.Add(new PullRequest("eCIS Systems Automation", "Cats", "https://stash/projects/ESA/repos/cats/pull-requests/8/overview", DateTime.Now.AddDays(-6)));
+                            // End Replace with Stash checking code
 
-                        // ### Below is commented out as sample of writing data to log and email ###
+                            Utils.SendEmail(pullRequests);
+                            Logger.Info($"Finished Checking {repoUrl}...");
+                        }
 
-
-
-                        //if (needToNag)
-                        //{
-                        //    List<string> errorMessages = new List<string>();
-                        //    List<string> emailErrorMessages = new List<string>();
-
-                        //    int errorCount = 0;
-
-                        //    //foreach (DataRow row in ds.Tables[0].Rows)
-                        //    //{
-                        //    //    string message = row["Message"].ToString();
-                        //    //    Regex regex = new Regex("not updated by '(?<agentModule>.*?)' as expected");
-                        //    //    string agentModule = regex.Match(message).Groups["agentModule"].Value.ToUpper();
-
-                        //    //    regex = new Regex("Domain:? ?'(?<Domain>.*?)'");
-                        //    //    var regexMatch = regex.Match(message);
-                        //    //    string DomainToReport = string.IsNullOrEmpty(regexMatch.Groups["Domain"].ToString()) ? row["Domain"].ToString().ToUpper() : regexMatch.Groups["Domain"].ToString().ToUpper();
-
-                        //    //    regex = new Regex("Machine:? ?'(?<Machine>.*?)'");
-                        //    //    regexMatch = regex.Match(message);
-                        //    //    string MachineToReport = string.IsNullOrEmpty(regexMatch.Groups["Machine"].ToString()) ? row["Server"].ToString().ToUpper() : regexMatch.Groups["Machine"].ToString().ToUpper();
-
-                        //    //    StringBuilder emailErrorMessage = new StringBuilder();
-                        //    //    string extraErrorMsg = string.Empty;
-
-                        //    //    string errorBackgroundColor = errorCount % 2 == 0 ? "FFFFFF" : "EEEEEE";
-                        //    //    errorMessages.Add($"{row["ModuleTested"]}: {row["Domain"]}: {row["Server"]}:\n{message}\n{extraErrorMsg}");
-                        //    //    emailErrorMessage.Append($"<div style='background-color:#{errorBackgroundColor};border:1px dotted gray; margin-bottom: .25em; ppadding:.25em'>");
-                        //    //    emailErrorMessage.Append($"<p style='margin: .25em'><strong>{row["ModuleTested"]} - {DomainToReport} - {MachineToReport}<br></strong></p>");
-                        //    //    emailErrorMessage.Append($"<p style='margin: .25em'>{row["starttime"]} - {message}");
-                        //    //    if (!string.IsNullOrEmpty(extraErrorMsg))
-                        //    //    {
-                        //    //        emailErrorMessage.Append("<br>{extraErrorMsg}");
-                        //    //    }
-                        //    //    emailErrorMessage.Append($"<br>Reported By: {row["ModuleTested"]} - {row["Domain"]} - {row["Server"]}</p>");
-                        //    //    emailErrorMessage.Append($"</div>");
-                        //    //    emailErrorMessages.Add(emailErrorMessage.ToString());
-                        //    //    errorCount++;
-
-                        //    //}
-                        //    //Logger.Error($"The following {errorCount} error messages were found for Compass HealthMonitor:\n{string.Join(Environment.NewLine, errorMessages)}");
-                        //    //SendEmail("Compass Health Monitor Errors", emailErrorMessages, monitorWakeSpan.TotalHours, $"{startingTime.Value} - {endingTime.Value}", errorCount);
-                        //}
-                        //else
-                        //{
-                        //    Logger.Info($"No errors found for StartTime: {startingTime.Value} - {endingTime.Value}");
-                        //}
                         Logger.Info("Processing complete...");
                     }
                     catch (Exception e)
@@ -119,42 +73,6 @@ namespace StashNag
                 Logger.Error($"Failed to initialize! {e.ToString()}");
             }
         }
-
-        private void SendEmail(string subject, List<string> emailErrorMessages, double hours, string timeSpan, int errorCount)
-        {
-           if (Environment.UserInteractive)
-            {
-                subject = $"Debug - {subject}";
-            }
-
-            string body = "<div style='margin: 0;font-size:14px;line-height:17px;color:#1F497D;font-family:Arial, Helvetica, sans-serif'>" +
-                            $"<p style='margin: 0;font-size: 22px;line-height: 26px'><strong>{subject}</strong></p>" +
-                            $"<p style='margin: 0;'>The following {errorCount} errors were found for the {hours} hour time span: {timeSpan}<br><br></p>" +
-                            string.Join("",emailErrorMessages) + "</div>";
-
-            MailMessage mail = new MailMessage()
-            {
-                From = new MailAddress(FromEmailAddress),
-                Subject = subject,
-                Body = body,
-                IsBodyHtml = true
-            };
-            mail.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(body, null, MediaTypeNames.Text.Html));
-            mail.To.Add(new MailAddress(ToEmailAddress));
-
-            try
-            {
-                using (SmtpClient smpt = new SmtpClient())
-                {
-                    smpt.Host = SmtpServer;
-                    smpt.Timeout = 15000;
-                    smpt.Send(mail);
-                }
-            }
-            catch(Exception e)
-            {
-                Logger.Error($"Unable to send mail. {e.ToString()}");
-            }
-        }
     }
+
 }
